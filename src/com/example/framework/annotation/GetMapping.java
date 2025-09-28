06554bafe7725bef5182773f45ba6311bf5ad0e1
TheAizen <andypatrickpro@gmail.com>
2025-09-28 20:48:43 +0300

    delete des inutiles

--- src/com/example/framework/annotation/GetMapping.java
+++
@@ -1,9 +0,0 @@
-package com.example.framework.annotation;
-
-import java.lang.annotation.*;
-
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.METHOD)
-public @interface GetMapping {
-    String value();
-}
